0e9bfbfc1d77f81d18ab886f96db4ca1244cfc38
clux <126624586+cluxvrontt@users.noreply.github.com>
2023-12-15 14:25:00 -0600

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -2,7 +2,7 @@
 
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "  bare: "ultraviolet-static-cluxvrontts-projects.vercel.app",",
+  bare: "ultraviolet-static-cluxvrontts-projects.vercel.app",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
